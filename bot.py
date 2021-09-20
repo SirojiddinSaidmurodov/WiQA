dcddb207a46b6ca013d90b6491e36593d662523f
Sirojiddin Saidmurodov <siroj.serj15@outlook.com>
2021-09-21 02:47:37 +0300

    add: methods to set context and ask question
    Took 1 hour 2 minutes

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
-import os
-
 import dotenv
+import logging
+import os
 import telebot
 from deeppavlov import build_model
 from deeppavlov.core.common.file import read_json
@@ -8,26 +8,34 @@ from deeppavlov.core.common.file import read_json
 dotenv.load_dotenv()
 bot_token = os.environ.get("bot_token")
 
-bot = telebot.TeleBot(bot_token)
+logging.basicConfig(level=logging.INFO)
 
+bot = telebot.TeleBot(bot_token)
 model_config = read_json('config.json')
 model = build_model(model_config, download=True)
-context = ''
+context: str = ""
+
 
 @bot.message_handler(commands=['help', 'h'])
 def handle_help(message):
     bot.send_message(message.from_user.id, "Need help?")
 
 
-@bot.message_handler(commands=['context','c'])
+@bot.message_handler(commands=['c'])
 def parse_context(message):
-    context = message.text
+    global context
+    context = message.text[3:]
+    bot.send_message(message.from_user.id, "Accepted")
 
 
-@bot.message_handler(commands=['context','c'])
+@bot.message_handler(commands=['q'])
 def answer_question(message):
-    question:str = message.text
-    answers = model([context],question)
+    question: str = message.text
+    logging.debug(context)
+    answers = model([context], [question])
+    logging.info(answers)
+    bot.send_message(message.from_user.id, answers)
+
 
 @bot.message_handler()
 def handle_message(message):
@@ -36,6 +44,8 @@ def handle_message(message):
 
 while True:
     try:
+        logging.debug("Started")
+        logging.info(model(['DeepPavlov is library for NLP and dialog systems.'], ['What is DeepPavlov?']))
         bot.polling(none_stop=True)
-    except e:
+    except Exception as e:
         print(e)
