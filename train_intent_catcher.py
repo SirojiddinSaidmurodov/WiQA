3d6476b9954ee23c96dfda0902507a124aa85ed0
Sirojiddin Saidmurodov <siroj.serj15@outlook.com>
2021-12-08 11:13:12 +0300

    edit intent catcher train epochs parameter
    Took 2 minutes

--- train_intent_catcher.py
+++ train_intent_catcher.py
@@ -2,11 +2,11 @@ import json
 
 from deeppavlov import train_model
 
-train_data = json.load(open("files/downloads/intent_catcher_data/train.json"))
-config = json.load(open('intent_catcher_config.json'))
-print(config)
-print(train_data)
-config['chainer']['pipe'][1]['number_of_intents'] = len(train_data.keys())
-config['train']['epochs'] = 40
-model = train_model(config)
-print("Trained!")
+if __name__ == '__main__':
+    train_data = json.load(open("files/downloads/intent_catcher_data/train.json"))
+    config = json.load(open('intent_catcher_config.json'))
+    print(config)
+    print(train_data)
+    config['chainer']['pipe'][1]['number_of_intents'] = len(train_data.keys())
+    model = train_model(config)
+    print("Trained!")
