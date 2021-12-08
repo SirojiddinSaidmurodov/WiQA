dca80bfbde5bb155ac6485c28aa30494ab91389b
Sirojiddin Saidmurodov <siroj.serj15@outlook.com>
2021-12-08 11:11:36 +0300

    add intent catcher dedicated server
    Took 5 hours 40 minutes

---
+++ intent_catcher_server.py
@@ -0,0 +1,20 @@
+import logging
+
+import rpyc
+from deeppavlov import build_model
+
+logging.basicConfig(level=logging.DEBUG)
+intent_catcher_model = build_model('intent_catcher_config.json')
+
+
+class CalculatorService(rpyc.Service):
+    def exposed_get_intent(self, message):
+        return intent_catcher_model([message])[0]
+
+
+if __name__ == "__main__":
+    from rpyc.utils.server import OneShotServer
+
+    logging.debug("Running...")
+    t = OneShotServer(CalculatorService, port=18861)
+    t.start()
--- simple_intent_example.py
+++ simple_intent_example.py
@@ -2,26 +2,25 @@ import logging
 import os
 
 import dotenv
+import rpyc as rpyc
 import telebot
-from deeppavlov import build_model
 
 dotenv.load_dotenv()
 bot_token = os.environ.get("bot_token")
 
 logging.basicConfig(level=logging.DEBUG)
 
-intent_catcher_model = build_model('intent_catcher_config.json')
+intent_catcher = rpyc.connect("localhost", 18861)
 
 bot = telebot.TeleBot(bot_token)
 
 
 @bot.message_handler()
 def handle_message(message):
-    bot.send_message(message.from_user.id, intent_catcher_model([message.text, ])[0])
+    bot.send_message(message.from_user.id, intent_catcher.root.get_intent(message.text))
 
 
 if __name__ == '__main__':
-
     try:
         logging.info("Started")
         bot.polling()
